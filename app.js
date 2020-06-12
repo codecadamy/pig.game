dea57b723938caf8fecc09d661b49090baf4c2c4
codecadamy <ayushman.jha@yahoo.com>
2020-06-12 20:55:09 +0545

    second iteration

--- app.js
+++ app.js
@@ -9,16 +9,38 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-//Generate Random Number between 1 and 6
-dice = Math.floor(Math.random() * 6) + 1;
-
-//Updating the current score
-document.querySelector("#current-" + activePlayer).textContent = dice;
-
+// Hiding the Dice when it starts
 document.querySelector(".dice").style.display = "none";
+
+// Resetting all the score values to 0
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var dice, diceDOM;
+
+  //Generate Random Number between 1 and 6
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  //Displaying the Dice
+  diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "img/dice-" + dice + ".png";
+
+  //Updating the current score if not 1
+  if (dice > 1) {
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Next Player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  }
+});
