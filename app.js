3790aef1795f7a986cb7c1e28009945a56eaf3a5
codecadamy <ayushman.jha@yahoo.com>
2020-06-13 14:26:44 +0545

    first iteration

--- app.js
+++ app.js
@@ -9,38 +9,94 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-// Hiding the Dice when it starts
-document.querySelector(".dice").style.display = "none";
-
-// Resetting all the score values to 0
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+init();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  var dice, diceDOM;
+  if (gamePlaying) {
+    var dice, diceDOM;
 
-  //Generate Random Number between 1 and 6
-  dice = Math.floor(Math.random() * 6) + 1;
+    //Generate Random Number between 1 and 6
+    dice = Math.floor(Math.random() * 6) + 1;
 
-  //Displaying the Dice
-  diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  diceDOM.src = "img/dice-" + dice + ".png";
+    //Displaying the Dice
+    diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "img/dice-" + dice + ".png";
 
-  //Updating the current score if not 1
-  if (dice > 1) {
-    roundScore += dice;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-  } else {
-    // Next Player
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //Updating the current score if not 1
+    if (dice > 1) {
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
   }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  if (gamePlaying) {
+    // Add current Score to Global Score
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    // Check if player have won the game
+    if (scores[activePlayer] >= 25) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector("player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector("player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      // Call next player
+      nextPlayer();
+    }
+  }
+});
+
+function nextPlayer() {
+  // Next Player
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlaying = true;
+
+  document.querySelector(".dice").style.display = "none";
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
